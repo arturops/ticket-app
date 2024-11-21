0c006d2f430ab7781e3adeb248b691a4ed7f76e7
Arturo Parrales <arturo.ps14@gmail.com>
2024-11-20 23:12:18 -0500

    Added error handler in routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signoutRouter } from './routes/signout';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
@@ -14,6 +15,7 @@ app.use(currentUserRouter);
 app.use(signoutRouter);
 app.use(signinRouter);
 app.use(signupRouter);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('==> Auth - listening on 3000');
