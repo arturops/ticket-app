0f593583e4a65d7fe71b1d8213daf24575165910
Arturo Parrales <arturo.ps14@gmail.com>
2024-11-20 23:13:06 -0500

    Threw errors in signup to test them

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,30 +1,35 @@
 import express, { Request, Response } from 'express';
 import { svcUrl } from '../config';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/db-connection-error';
 
 const router = express.Router();
 
 const route = svcUrl.concat('/signup');
 
 router.post(
-  route, 
+  route,
   [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password')
       .trim()
-      .isLength({min: 4, max: 20})
+      .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 to 20 characters'),
-  ], 
+  ],
   (req: Request, res: Response) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty()){
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
       // send array of json objects describing each error if > 1
-      res.status(400).send(errors.array());
-      return
+      //res.status(400).send(errors.array());
+      //return
     }
-    const {email, password }: {email: string, password: string} = req.body;
+    throw new DatabaseConnectionError();
+    const { email, password }: { email: string; password: string } = req.body;
     console.log('Creating user ...');
     res.send();
-});
+  }
+);
 
 export { router as signupRouter };
