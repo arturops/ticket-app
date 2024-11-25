8328c03b6611834f166adf5f516434633f7dd14d
Arturo Parrales <arturo.ps14@gmail.com>
2024-11-25 18:54:54 +0900

    Started mongo in auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signoutRouter } from './routes/signout';
 import { signinRouter } from './routes/signin';
@@ -26,6 +27,20 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
+const start = async () => {
+  //not localhost because we are using k8s service auth-mongo-svc
+  // ALSO, we add a db 'auth'
+  try {
+    await mongoose.connect('mongodb://auth-mongo-svc:27017/auth');
+    console.log('Auth connected to mongo db');
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 app.listen(3000, () => {
   console.log('==> Auth - listening on 3000');
 });
+
+// mongoose starts after our app starts
+start();
