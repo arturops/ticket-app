e3aae68d4dbf1f4b8a559bf4296ec9b9cac77e66
Arturo Parrales <arturo.ps14@gmail.com>
2024-12-06 20:53:53 +0900

    Added jwt secret key env to auth config

--- auth/src/config.ts
+++ auth/src/config.ts
@@ -1,14 +1,21 @@
 interface Config {
   baseUrl: string;
   apiVersion: string;
+  jwtSigningKey: string | null;
 }
 
 const config: Config = {
   apiVersion: process.env.API_VERSION || '/api/v0',
   baseUrl: process.env.BASE_URL || '/users',
+  jwtSigningKey: process.env.JWT_KEY || null,
 };
 
+if (!config.jwtSigningKey) {
+  throw new Error('JWT_KEY must be defined');
+}
+
 export const svcUrl = config.apiVersion.concat(config.baseUrl);
+export const jwtSigningKey = config.jwtSigningKey;
 
 // export default if you want to avoid import { config } from ...
 export default config;
