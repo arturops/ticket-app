43ead69cdf31256c844429f7a241b54738a4d37a
Arturo Parrales <arturo.ps14@gmail.com>
2025-01-06 22:55:44 +0900

    Added propagation of cookie info whether in server-side or browser side

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,49 @@
-const index = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
-export default index;
+LandingPage.getInitialProps = async ({ req }) => {
+  const urlCurrentUser = '/api/v0/users/currentuser';
+  console.log(req.headers);
+  //hack to know if we are running on the server side
+  // of next js or client side
+  if (typeof window === 'undefined') {
+    // server side has no window defined
+    // we are on the server! and in k8s
+    // this means requests should go to different
+    // namespace ingress
+    console.log('server side');
+    const serviceName = 'ingress-nginx-controller';
+    const namespace = 'ingress-nginx';
+    const ingressHostname = 'tx-app.io';
+    const { data } = await axios.get(
+      `http://${serviceName}.${namespace}.svc.cluster.local${urlCurrentUser}`,
+      {
+        headers: req.headers,
+        // headers: {
+        //   Host: ingressHostname,
+        // },
+      }
+    );
+    // .catch((error) => {
+    //   console.log('Error: ', error?.message);
+    // });
+    return data;
+  } else {
+    // we are on the browser!
+    // request do not need special base url, simply
+    // route
+    console.log('browser side');
+    const { data } = await axios.get(urlCurrentUser);
+    // .catch((error) => {
+    //   console.log('Error: ', error?.message);
+    // });
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
