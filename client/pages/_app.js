62cbb75c7d1dd916509269590f20ef08de34d65f
Arturo Parrales <arturo.ps14@gmail.com>
2025-01-12 22:25:02 +0900

    Added the get initial props to be handled on main client app to pass down to other pages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Header {currentUser.email} </h1>
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+AppComponent.getInitialProps = async (appContext) => {
+  const axiosClient = buildClient(appContext.ctx);
+  const currentUserURL = 'api/v0/users/currentuser';
+  const { data } = await axiosClient.get(currentUserURL);
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    currentUser: data.currentUser,
+  };
+};
+
+export default AppComponent;
